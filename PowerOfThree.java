f3905aea67b71f953cd20f85e7eda26d90adf788
Hadassah Latchague <38115851+hlatchague1@users.noreply.github.com>
2021-02-11 09:13:07 -0800

    Successful submission

--- PowerOfThree.java
+++ PowerOfThree.java
@@ -1,11 +1,15 @@
 // Given an integer n, return true if it is a power of three otherwise return false
 
 class PowerOfThree{
-    public boolean isPowerOfThree(int n) {       
-        if(n == 0) return false;
+    public boolean isPowerOfThree(int n) {
+        if(n == 1) return true;
+        if(n == 0 || n % 2 == 0 || n % 3 != 0) return false;
         while(n > 1){
             if (n == 3) return true;
-            n = n / 3;  
+            if(n % 3 == 0) n = n / 3;
+            else{
+                return false;
+            }
         }
         return false;
     }
